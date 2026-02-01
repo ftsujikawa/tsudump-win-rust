81a05f9466088304c60369736c821693a1aaf057
Fumihito TSUJIKAWA <readme627@gmail.com>
2026-02-01 14:29:17 +0900

    fix: マージコンフリクトマーカーを削除し、グローバル変数iの宣言を修正

--- a.c
+++ a.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
+int i = 0;#include <stdio.h>
 
 void func() {
     printf("func\n");
 }
 
 int main() {
+    int i = 0;
     printf("Hello, World!\n");
-<<<<<<< HEAD
-=======
     ++i;
     printf("i = %d\n", i);
->>>>>>> 3e9dd4bc4b0456396c16e8552c7e9018153a5f44
     func();
     return 0;
 }
